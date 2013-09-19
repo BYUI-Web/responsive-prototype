3c2a65238c43dbcdb0fa09f3ebad519c66850c0f
sloveland <samurai@samuelloveland.com>
2013-09-19 15:31:35 -0600

    deleting legacy code
    in order to make this repository more of a stand-alone prototype

--- js/content_swap.js
+++
@@ -1,9 +0,0 @@
-var activeIndex = "A_group_box";
-
-function toggleIndex(id) { 
-	var f = document.getElementById(activeIndex);
-	f.style.display = 'none';
-	var e = document.getElementById(id);
-	e.style.display = 'block';
-	activeIndex = id;	
-}
