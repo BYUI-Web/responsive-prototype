f40662384300a912dd8e10b11479dec86162affe
Matthew Sessions <shichgonrui@gmail.com>
2014-09-05 15:38:02 -0600

    Fixed some ids to classes and fixed bugs in the javascript

--- _includes/activities-events-table/activities-events-tables.js
+++ _includes/activities-events-table/activities-events-tables.js
@@ -6,18 +6,18 @@ $(document).ready(function () {
         }
 
         //collapse or expand the div
-        var collapsed = $(this).find("div#eventDescription").html();
+        var collapsed = $(this).find("div.eventDescription").html();
 
         if (collapsed.indexOf("...") > 0) {
             collapsed = collapsed.replace("...", "");
         } else {
-            collapsed = collapsed.slice(0, collapsed.lastIndexOf(" <")) + "...";
+            collapsed += "...";
         }
 
-        $(this).find("div#eventDescription").html(collapsed);
+        $(this).find("div.eventDescription").html(collapsed);
 
         //collapse or expand the div
-        $(this).find("div#collapsed").slideToggle("medium");
+        $(this).find("div.collapsed-text").slideToggle("medium");
 
         //toggle the class
         var sign = $(this).find("div.circle").find("div.sign");
