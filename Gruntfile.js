8233d81ef5fe765cddc7a776b87cf63dd5977735
Matthew Sessions <shichgonrui@gmail.com>
2014-09-08 11:46:08 -0600

    Completed component with dummy content

--- Gruntfile.js
+++ Gruntfile.js
@@ -90,7 +90,7 @@ module.exports = function (grunt) {
             },
 
             jekyll: {
-                files: ["assets/**/*", "**/*.html"],
+                files: ["assets/**/*", "_includes/**/*.html", "pages/**/*.html"],
                 tasks: ["jekyll"],
                 options: {
                     livereload: true
