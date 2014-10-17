2ff82013e3780e937ea4c86729aaf955417b420a
Matthew Sessions <shichongrui@gmail.com>
2014-10-17 13:44:38 -0600

    Got the home page desktop done.  Finishing up loose ends here and there

---
+++ _includes/home-image-rotator/home-image-rotator.js
@@ -0,0 +1,29 @@
+(function () {
+    var homeCarousel = document.querySelector("#home-carousel");
+    if (homeCarousel) {
+        $(".active").css("opacity", 1);
+
+        $(homeCarousel).on("slide.bs.carousel", function (e) {
+            var next;
+            
+            console.log(e.direction);
+            
+            if (e.direction === "right") {
+                next = $(this).find(".active").prev();
+                next = (next.length) ? next : $(this).find(".item:last-child");
+            } else {
+                next = $(this).find(".active").next();
+                next = (next.length) ? next : $(this).find(".item:first-child");
+            }
+
+            $(next).animate({
+                opacity: 1
+            }, 500);
+
+
+            $(this).find(".active").animate({
+                opacity: 0
+            }, 500);
+        });
+    }
+})();
