f9a378ae13c76c534de52629ed2c64100e9ca571
Matthew Sessions <shichongrui@gmail.com>
2014-10-31 12:49:37 -0600

    Added a multi-task for uglify to prevent uglification on dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,13 @@ module.exports = function (grunt) {
     "use strict";
 
     require("load-grunt-tasks")(grunt);
-    var taskList = ["less:pages", "less:global", "autoprefixer", "jekyll:build", "uglify"],
+    var taskList = ["less:pages", "less:global", "autoprefixer", "jekyll:build"],
         watchList = taskList.concat([]),
         banner = "/* DO NO EDIT THIS FILE.  This file is built from a source file.  Edit that file instead. */";
 
     taskList.push("cssmin");
+    taskList.push("uglify:js");
+    watchList.push("uglify:jsDev");
     watchList.push("connect:server");
     watchList.push("watch");
 
@@ -94,6 +96,15 @@ module.exports = function (grunt) {
             js: {
                 options: {
                     compress: true
+                },
+
+                files: {
+                    "assets/js/main.min.js": ["pages/**/*.js", "_includes/**/*.js"]
+                }
+            },
+            jsDev: {
+                options: {
+                    compress: false
                 },
 
                 files: {
---
+++ _includes/nav/nav.js
@@ -0,0 +1,9 @@
+(function() {
+    var nav = document.querySelector(".department-nav");
+    
+    if (nav) {
+        var more = nav.querySelector(".department-nav__more");
+        
+        more.style.display = "block";
+    }
+})();
