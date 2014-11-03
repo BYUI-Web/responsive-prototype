75fe0fa53a563ef755c315bd3da5f21f57dcb389
Matthew Sessions <shichongrui@gmail.com>
2014-11-03 11:01:33 -0700

    Got multi-line department nav working.  It will only do it's magic when javascript is enabled.  But it works by hiding all of the nav elements that are not on the first line and then setting the max-height for the nav.  Then whenever the more button is clicked, it will expand the nav and show all of the next-line nav elements

--- _includes/nav/nav.js
+++ _includes/nav/nav.js
@@ -1,9 +1,38 @@
-(function() {
-    var nav = document.querySelector(".department-nav");
-    
+(function () {
+    var nav = document.querySelector(".department-nav"),
+        state = false;
+
     if (nav) {
-        var more = nav.querySelector(".department-nav__more");
-        
-        more.style.display = "block";
+        var more = nav.querySelector(".department-nav__more"),
+            moreThanOneLine = hasMoreThanOneLine();
+
+        if (moreThanOneLine) {
+            nav.className += " department-nav--more-lines";
+        }
+
+        more.addEventListener("click", function (e) {
+            Array.prototype.forEach.call(document.querySelectorAll(".next-line"), function(element) {
+               element.style.display = (state) ? "none" : "block"; 
+            });
+            nav.style.maxHeight = (state) ? "50px" : "300px";
+            state = !state;
+            e.preventDefault();
+        });
+
+    }
+
+    function hasMoreThanOneLine() {
+        var navItems = nav.querySelectorAll(".navbar-collapse li"),
+            numItems = navItems.length,
+            moreThanOneLine = false;
+
+        for (var i = 0; i < numItems; i++) {
+            moreThanOneLine = moreThanOneLine || navItems[i].offsetTop > 0;
+            if (moreThanOneLine) {
+                navItems[i].className += " next-line";
+            }
+        }
+
+        return moreThanOneLine;
     }
 })();
