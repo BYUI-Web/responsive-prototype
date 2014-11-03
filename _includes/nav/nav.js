7de4e4b52f333da2779cc1631ff28e060dadd9e8
Matthew Sessions <shichongrui@gmail.com>
2014-11-03 13:10:01 -0700

    Made two changes.  The first is the badge being used for the dropdown.  It now is circular.  The second change is a bug fix for the navbar when going from collapsed open to larger screens.

--- _includes/nav/nav.js
+++ _includes/nav/nav.js
@@ -1,12 +1,12 @@
 (function () {
     var nav = document.querySelector(".department-nav"),
         state = false;
-    
+
     if (nav) {
         var more = nav.querySelector(".department-nav__more");
-        
+
         showMore();
-        
+
         more.addEventListener("click", function (e) {
             Array.prototype.forEach.call(document.querySelectorAll(".next-line"), function (element) {
                 element.style.display = (state) ? "none" : "block";
@@ -15,7 +15,7 @@
             state = !state;
             e.preventDefault();
         });
-        
+
         window.addEventListener("resize", showMore);
     }
 
@@ -28,23 +28,24 @@
             $(nav).removeClass("department-nav--more-lines");
         }
 
-
-
     }
 
     function hasMoreThanOneLine() {
         var navItems = nav.querySelectorAll(".navbar-collapse li"),
             numItems = navItems.length,
             moreThanOneLine = false;
-        
+
         $(navItems).removeClass("next-line").css("display", "");
-        for (var i = 0; i < numItems; i++) {
-            moreThanOneLine = moreThanOneLine || navItems[i].offsetTop > 0;
-            if (moreThanOneLine) {
-                $(navItems[i]).addClass("next-line");
+
+        if (!$(".department-nav .navbar-toggle:visible").length) {
+            for (var i = 0; i < numItems; i++) {
+                moreThanOneLine = moreThanOneLine || navItems[i].offsetTop > 0;
+                if (moreThanOneLine) {
+                    $(navItems[i]).addClass("next-line");
+                }
             }
+            nav.style.height = "50px";
         }
-        nav.style.height = "50px";
         state = false;
 
         return moreThanOneLine;
